2683c414c79f29a7f4c124db4716f2ee5ac6e049
officeAccount <ashutosh@discoverwebtech.com>
2023-04-15 01:12:04 +0530

    initial

---
+++ controllers/User.js
@@ -0,0 +1,311 @@
+import { User } from "../models/users.js";
+import { sendMail } from "../utils/sendMail.js";
+import { sendToken } from "../utils/sendToken.js";
+import cloudinary from "cloudinary";
+import fs from "fs";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const avatar = req.file.path;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists!" });
+    }
+    const otp = Math.floor(Math.random() * 10000);
+    const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+      folder: "todoApp",
+    });
+    fs.rmSync("./temp", { recursive: true });
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar: {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      },
+      otp,
+      otp_expiry: new Date(Date.now() + 5 * 60 * 1000),
+    });
+    await sendMail(email, "verify your account", `your OTP is ${otp}`);
+
+    sendToken(
+      res,
+      user,
+      200,
+      "OTP sent to your email, please verify your account."
+    );
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const verify = async (req, res) => {
+  try {
+    const otp = Number(req.body.otp);
+    console.log("otp", otp);
+
+    const user = await User.findById(req.user._id);
+
+    console.log("user", user);
+
+    if (user.otp !== otp || user.otp_expiry < Date.now()) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invalid OTP or has been expired" });
+    }
+    user.verified = true;
+    user.otp = null;
+    user.otp_expiry = null;
+
+    await user.save();
+
+    sendToken(res, user, 200, "Account Verified!");
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log("req", req.body);
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter all fields!" });
+    }
+    let user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password!" });
+    }
+
+    console.log("user", user);
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password!!" });
+    }
+
+    sendToken(res, user, 200, "Login Successfull!");
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", null, {
+        expires: new Date(Date.now()),
+      })
+      .json({ success: true, message: "Logged out successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const addTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    user.tasks.push({
+      title,
+      description,
+      completed: false,
+      createdAt: new Date(Date.now()),
+    });
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Task added successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const removeTask = async (req, res) => {
+  try {
+    const { taskId } = req.params;
+
+    const user = await User.findById(req.user._id);
+
+    user.tasks = user.tasks.filter(
+      (task) => task._id.toString() !== taskId.toString()
+    );
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Task removed successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { taskId } = req.params;
+
+    const user = await User.findById(req.user._id);
+
+    user.task = user.tasks.find(
+      (task) => task._id.toString() !== taskId.toString()
+    );
+
+    user.task.completed = !user.task.completed;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Task updated successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const getMyProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    sendToken(res, user, 201, `welcome back ${user.name}`);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const { name } = req.body;
+
+    const avatar = req.file.path;
+
+    if (name) user.name = name;
+    if (avatar) {
+      await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+      const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+        folder: "todoApp",
+      });
+      fs.rmSync("./temp", { recursive: true });
+      user.avatar = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    }
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Profile Updated Successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const updatePassword = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please Enter All Fields!" });
+    }
+
+    const isMatch = await user.comparePassword(oldPassword);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Old Password!" });
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Password Updated Successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+
+    const otp = Math.floor(Math.random() * 10000);
+
+    user.resetPasswordOtp = otp;
+    user.resetPasswordOtpExpiry = Date.now() + 10 * 60 * 1000;
+
+    await user.save();
+
+    const message = `Your OTP for resetting the password ${otp}. If you did not request for this, please ignore this email.`;
+
+    await sendMail(email, "Request for Resetting Password", message);
+
+    res
+      .status(200)
+      .json({ success: true, message: `OTP sent to ${email}  Successfully!` });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { otp, newPassword } = req.body;
+    const user = await User.findOne({
+      resetPasswordOtp: otp,
+      resetPasswordOtpExpiry: { $gt: Date.now() },
+    }).select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "otp invalid or has been expired." });
+    }
+    user.password = newPassword;
+    user.resetPasswordOtp = null;
+    user.resetPasswordOtpExpiry = null;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: `Password Changed Successfully!` });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
---
+++ routers/User.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  addTask,
+  forgotPassword,
+  getMyProfile,
+  login,
+  logout,
+  register,
+  removeTask,
+  resetPassword,
+  updatePassword,
+  updateProfile,
+  updateTask,
+  verify,
+} from "../controllers/User.js";
+import { isAuthenticated } from "../middleware/auth.js";
+import multer from "multer";
+const upload = multer({ dest: "temp" });
+const router = express.Router();
+
+router.route("/register").post(upload.single("avatar"), register);
+router.route("/verify").post(isAuthenticated, verify);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/newtask").post(isAuthenticated, addTask);
+router
+  .route("/task/:taskId")
+  .get(isAuthenticated, updateTask)
+  .delete(isAuthenticated, removeTask);
+
+router.route("/me").get(isAuthenticated, getMyProfile);
+router.route("/updateprofile").put(isAuthenticated, updateProfile);
+router.route("/updatepassword").put(isAuthenticated, updatePassword);
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/resetpassword").put(resetPassword);
+
+export default router;
