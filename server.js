9a1bdf5af67b09d16cee8d888424ed4f92e6ddcc
Ashutosh Raghuvanshi <101924736+Ashutosh-DWT@users.noreply.github.com>
2023-04-15 11:46:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import { app } from "./app.js";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
 
-config();
+dotenv.config();
 cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
