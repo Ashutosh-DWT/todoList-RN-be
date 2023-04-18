7e5e67e976783c3ba7da75dc48549db8b95a1d39
officeAccount <ashutosh@discoverwebtech.com>
2023-04-18 23:40:38 +0530

    ..

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
-import path from "path"
 
 dotenv.config();
 cloudinary.v2.config({
@@ -14,8 +13,4 @@ connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running at port " + process.env.PORT);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
 });
