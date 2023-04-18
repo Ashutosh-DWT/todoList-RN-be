01c2d12d8e1a8edabb950f377a79813ec828bbf7
officeAccount <ashutosh@discoverwebtech.com>
2023-04-19 00:05:15 +0530

    ..

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import cors from "cors";
 export const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true , limit: "100mb"}));
 app.use(cookieParser());
 app.use("/", User);
 app.use(
